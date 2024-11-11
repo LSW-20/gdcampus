70fc69eec7d3d81f3eb7e08dcc5acaca42a1c3df
SANGWOO <kuiki700@gmail.com>
2024-11-11 17:52:57 +0900

    feat: 채팅방 기능 구현 중
    채팅방 기능 구현 중

---
+++ src/main/java/com/br/gdcampus/service/ChatServiceImpl.java
@@ -0,0 +1,11 @@
+package com.br.gdcampus.service;
+
+import org.springframework.stereotype.Service;
+
+//@RequiredArgsConstructor
+@Service
+public class ChatServiceImpl implements ChatService {
+
+	//private final MemberDao memberDao;
+	
+}
