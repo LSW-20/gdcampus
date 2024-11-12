f1aeeb012763f0918660a617c4619049ba6bd71c
chlqhrua <83631894+chlqhrua@users.noreply.github.com>
2024-11-12 20:50:59 +0900

    Update ApprovalController.java
    단순controller 주석 추가

--- src/main/java/com/br/gdcampus/controller/ApprovalController.java
+++ src/main/java/com/br/gdcampus/controller/ApprovalController.java
@@ -2,8 +2,9 @@ package com.br.gdcampus.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.br.gdcampus.util.PagingUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,17 +12,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Controller
 public class ApprovalController {
+//	private final 
+	private final PagingUtil pagingUtil;
 	
-	
+	//결재대기함 list 단순 페이지요청
 	@GetMapping("/todo.do")
 	public void todoPage() {}
 	
+	//기안서페이지 단순 페이지요청
 	@GetMapping("/simpleDraft.do")
 	public void simpleDraftPage() {}
 	
+	//품의서 단순 페이지요청
 	@GetMapping("/purchaseDraft.do")
 	public void purchaseDraftPage() {}
 	
+	//작성페이지 요청
 	@GetMapping("/regist.do")
 	public void registPage() {}
+	
+
+	
+	
 }
