de27dc30ab96d9076e41558742da87b371114c2f
hohaoh <gksmf3490@naver.com>
2024-11-12 09:49:03 +0900

    Controller 추가(수업,계획서,개설신청,사원관리)

---
+++ src/main/java/com/br/gdcampus/controller/ClassController.java
@@ -0,0 +1,8 @@
+package com.br.gdcampus.controller;
+
+/**
+ * 수업 관련 컨트롤러
+ */
+public class ClassController {
+
+}
---
+++ src/main/java/com/br/gdcampus/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.br.gdcampus.controller;
+
+/**
+ * 인사팀 사원/교수관리 컨트롤러
+ */
+public class UserController {
+
+	
+}
