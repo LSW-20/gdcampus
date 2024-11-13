c2fa43241bc0d297039be93ebdfdff034c8b03db
hohaoh <118319662+hohaoh@users.noreply.github.com>
2024-11-13 09:42:12 +0900

    회원관리controller,mapper추가

--- src/main/java/com/br/gdcampus/controller/UserController.java
+++ src/main/java/com/br/gdcampus/controller/UserController.java
@@ -2,6 +2,7 @@ package com.br.gdcampus.controller;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.br.gdcampus.service.UserService;
@@ -25,5 +26,7 @@ public class UserController {
 	private final FileUtil fileUtil;
 	private final PagingUtil pagingUtil;
 	
+	@GetMapping("/staff/list.do")
+	public void todoPage() {}
 	
 }
