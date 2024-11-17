509901aed96c3e943c5718a655a459d7916c3bd0
hohaoh <118319662+hohaoh@users.noreply.github.com>
2024-11-17 18:24:15 +0900

    개설신청 내용 추가

--- src/main/java/com/br/gdcampus/dao/ClassDao.java
+++ src/main/java/com/br/gdcampus/dao/ClassDao.java
@@ -1,7 +1,14 @@
 package com.br.gdcampus.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.br.gdcampus.dto.ClassDto;
+import com.br.gdcampus.dto.PageInfoDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,7 +19,10 @@ public class ClassDao {
 	private final SqlSessionTemplate sqlSession;
 	
 //--------------------------------------개설신청-교무팀 시작--------------------------------------------
-	
+	public List<ClassDto> selectClassList(Map<String, String> search, PageInfoDto pi) {
+		RowBounds rowBounds = new RowBounds((pi.getCurrentPage() - 1) * pi.getBoardLimit() ,pi.getBoardLimit());
+		return sqlSession.selectList("classMpper.selectStaffClassList",search,rowBounds);
+	}
 //--------------------------------------개설신청-교무팀 끝---------------------------------------------	
 	
 //--------------------------------------개설신청-교수 시작---------------------------------------------
