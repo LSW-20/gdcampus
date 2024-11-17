e571be4c4a664602d13ba9a7a13e413e8df58e6a
hohaoh <118319662+hohaoh@users.noreply.github.com>
2024-11-17 22:39:22 +0900

    개설신청 관련 내용 추가

--- src/main/java/com/br/gdcampus/controller/ClassController.java
+++ src/main/java/com/br/gdcampus/controller/ClassController.java
@@ -1,8 +1,16 @@
 package com.br.gdcampus.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+
 /**
  * 수업 관련 컨트롤러
  */
+@RequestMapping("/class")
+@RequiredArgsConstructor
+@Controller
 public class ClassController {
 
 }
