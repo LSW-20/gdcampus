2e6256c549db3ff515d11a06e3118fcf01c26e47
hohaoh <118319662+hohaoh@users.noreply.github.com>
2024-11-18 10:57:38 +0900

    개설신청 관련 수정

--- src/main/java/com/br/gdcampus/service/ClassService.java
+++ src/main/java/com/br/gdcampus/service/ClassService.java
@@ -3,6 +3,7 @@ package com.br.gdcampus.service;
 import java.util.List;
 import java.util.Map;
 
+import com.br.gdcampus.dto.ClassDto;
 import com.br.gdcampus.dto.PageInfoDto;
 import com.br.gdcampus.dto.UserDto;
 
@@ -12,20 +13,21 @@ public interface ClassService {
 	
 	// 개설신청 목록 조회
 	int selectStaffOpningListCount(Map<String,String> search);
-	List<UserDto> selectStaffOpningList(Map<String,String> search, PageInfoDto pi);
+	List<ClassDto> selectStaffOpningList(Map<String,String> search, PageInfoDto pi);
 	// 개설신청 상세 조회
-	UserDto selectStaff(String userNo);
+	ClassDto selectStaffOpningDetail(String classCode);
 	// 개설신청 승인 처리(여부도 바뀌어야함 이건 트리거로?), 반려처리, 보완요청
-	int updateStaff(UserDto user);
+	int updateOpningStatus(ClassDto c);
 	
 //--------------------------------------개설신청-교무팀 끝---------------------------------------------	
 		
 //--------------------------------------개설신청-교수 시작---------------------------------------------
 	
 	// 개설신청 목록 조회
-	
+	int selectProfOpningListCount(Map<String,String> search);
+	List<UserDto> selectProfOpningList(Map<String,String> search, PageInfoDto pi);	
 	// 개설신청 상세 조회
-	
+	ClassDto selectProfOpningDetail(String classCode);
 	// 개설신청 상세 조회
 	
 	// 개설신청 수정(보완)
