bcd1b6ce7c8880b0af2968b5b6a7c27b89ca85bf
lsc7676 <allofyaeji@naver.com>
2024-11-19 17:20:00 +0900

    .

---
+++ src/main/java/com/br/gdcampus/controller/HoliController.java
@@ -0,0 +1,13 @@
+package com.br.gdcampus.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HoliController {
+	
+	
+	@GetMapping("/workstatus")
+	public void workstatus() {
+			
+		}
+		
+}
---
+++ src/main/java/com/br/gdcampus/dao/HoliDao.java
@@ -0,0 +1,7 @@
+package com.br.gdcampus.dao;
+
+public class HoliDao {
+	
+	
+	
+}
