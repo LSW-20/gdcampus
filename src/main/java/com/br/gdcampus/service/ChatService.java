16d184125a900495f10f255c6cefe3285c3d7c3b
SANGWOO <kuiki700@gmail.com>
2024-11-21 12:31:19 +0900

    feat : 채팅방 이름, 채팅방 참여 인원 목록 조회 기능 구현

--- src/main/java/com/br/gdcampus/service/ChatService.java
+++ src/main/java/com/br/gdcampus/service/ChatService.java
@@ -38,6 +38,9 @@ public interface ChatService {
 
 	// 채팅방 별 최근 메세지 조회 - 상우
 	List<MessageDto> recentMessage();
+
+	// 현재 채팅방(1개)에 대한 정보 조회 - 상우
+	ChatRoomDto selectChatRoomOne(String roomNo);
     
     
     // 채팅방 초대 - 상우
