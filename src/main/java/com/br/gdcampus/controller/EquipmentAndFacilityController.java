45ce884a43551c2b638a2318ffddfab9d819ddb1
SANGWOO <kuiki700@gmail.com>
2024-11-22 17:47:38 +0900

    feat : 비품, 시설 목록 조회 기능 구현중

--- src/main/java/com/br/gdcampus/controller/EquipmentAndFacilityController.java
+++ src/main/java/com/br/gdcampus/controller/EquipmentAndFacilityController.java
@@ -10,12 +10,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.br.gdcampus.dto.EquipmentDto;
 import com.br.gdcampus.dto.FacilityDto;
+import com.br.gdcampus.dto.PageInfoDto;
 import com.br.gdcampus.service.EquipmentAndFacilityService;
+import com.br.gdcampus.util.PagingUtil;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +29,10 @@ import lombok.RequiredArgsConstructor;
 public class EquipmentAndFacilityController {
 
 	private final EquipmentAndFacilityService equipmentAndFacilityService;
+	private final PagingUtil pagingUtil;
 	
 	private Logger logger = LoggerFactory.getLogger(ChatController.class);
+	  
 	
 	
 	/**
@@ -68,28 +73,42 @@ public class EquipmentAndFacilityController {
 	 * author : 상우
 	 */
 	@GetMapping("/selectList")
-	public String selectList(String equipment, String facility, RedirectAttributes ra) {
+	public String selectList(String equipment, String facility, RedirectAttributes ra, @RequestParam(value="page", defaultValue="1") int currentPage) {
 		
 		logger.debug("equipment값 : {}", equipment); // "비품 전체", "노트북", "키보드", "마우스", "의자"
 		logger.debug("facility값 : {}", facility);	 // "시설 전체", "회의실", "강당", "휴게실", "연구실"	 
+		logger.debug("page값 : {}", currentPage);
 		
 		Map<String, Object> responseMap = new HashMap<>();
+
 		
-		if(equipment == null) { // 시설 리스트 조회
+		if(equipment == null) { // 시설 리스트 조회의 경우
 			
-			//List<EquipmentDto> equipmentList = equipmentAndFacilityService.selectFacilityList(facility);
-			responseMap.put("구분", "시설");
-			//responseMap.put("equipmentList", equipmentList);
+			int listCount = equipmentAndFacilityService.selectFacilityListCount();
+		    PageInfoDto pi = pagingUtil.getPageInfoDto(listCount, currentPage, 5, 5);
 			
-		}else if(facility == null) { // 비품 리스트 조회
+			List<FacilityDto> list = equipmentAndFacilityService.selectFacilityList(pi, facility);
 			
-			//List<FacilityDto> facilityList = equipmentAndFacilityService.selectEquipmentList(equipment);
-			responseMap.put("구분", "비품");			
-			//responseMap.put("facilityList", facilityList);
+			responseMap.put("category", "시설");
+			responseMap.put("list", list);
+
+			ra.addFlashAttribute("responseMap", responseMap);	
+			ra.addFlashAttribute("pi", pi);
+
 			
+		}else if(facility == null) { // 비품 리스트 조회의 경우
+			
+			int listCount = equipmentAndFacilityService.selectEquipmentListCount();
+		    PageInfoDto pi = pagingUtil.getPageInfoDto(listCount, currentPage, 5, 5);
+			
+			List<EquipmentDto> list = equipmentAndFacilityService.selectEquipmentList(pi, equipment);	
+			
+			responseMap.put("category", "비품");
+			responseMap.put("list", list);
+			
+			ra.addFlashAttribute("responseMap", responseMap);	
+			ra.addFlashAttribute("pi", pi);
 		}
-		
-		ra.addFlashAttribute("responseMap", responseMap);
 		
 		return "redirect:/equipmentAndFacility/manage";
 	}
