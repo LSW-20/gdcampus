5668fb6a313b53571e409e83c6ff982501f5f12c
phn1389 <167429100+phn1389@users.noreply.github.com>
2024-11-25 02:17:18 +0900

    notice공지사항 관련

--- src/main/java/com/br/gdcampus/controller/NoticeController.java
+++ src/main/java/com/br/gdcampus/controller/NoticeController.java
@@ -1,14 +1,15 @@
 package com.br.gdcampus.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.br.gdcampus.dao.NoticeDao;
+import com.br.gdcampus.dto.NoticeDto;
+import com.br.gdcampus.service.NoticeService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,15 +17,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequestMapping("/board/notice")
 @RequiredArgsConstructor
-@Controller
+@Controller 
 public class NoticeController {
+  
+	@Autowired
+	private final NoticeService noticeService; 
 	
-	@GetMapping("list")
-	public void noticeList() {
-		List<NoticeDao> list = NoticeService.selectNoticeList();
-		log.debug("list:{}", list);
-		model.addAttribute("list", list);
-		
-	}
-	
-}
+  @GetMapping("/list") 
+  public void noticeList(Model model){ 
+	  List<NoticeDto> list = noticeService.selectNoticeList(); 
+	  model.addAttribute("noticeList", list);
+  
+  }
+  
+  
+ }
+ 
--- src/main/java/com/br/gdcampus/dao/NoticeDao.java
+++ src/main/java/com/br/gdcampus/dao/NoticeDao.java
@@ -3,6 +3,7 @@ package com.br.gdcampus.dao;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.br.gdcampus.dto.NoticeDto;
@@ -13,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @Repository
 public class NoticeDao {
 
+	@Autowired
 	private final SqlSessionTemplate sqlSession;
 	
 	public List<NoticeDto> selectNoticeList(){
