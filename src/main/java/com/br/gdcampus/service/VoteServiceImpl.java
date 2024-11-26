4d7fd9157bf605845eb6f2a69ad0ee155dfe5ee5
phn1389 <167429100+phn1389@users.noreply.github.com>
2024-11-26 18:30:09 +0900

    vote 생성

--- src/main/java/com/br/gdcampus/service/VoteServiceImpl.java
+++ src/main/java/com/br/gdcampus/service/VoteServiceImpl.java
@@ -1,5 +1,30 @@
+
 package com.br.gdcampus.service;
+import java.util.List;
+import java.util.Map;
 
-public class VoteServiceImpl {
+import org.springframework.stereotype.Service;
 
+import com.br.gdcampus.dto.VoteDto;
+
+@Service
+public interface VoteServiceImpl {
+
+    // 투표 게시글 목록 조회 (페이징 및 조건 검색)
+    List<VoteDto> selectVoteList(Map<String, Object> paramMap);
+
+    // 특정 투표 게시글 조회
+    VoteDto selectVoteById(String voteId);
+
+    // 투표 게시글 등록
+    int insertVote(VoteDto vote);
+
+    // 투표 게시글 수정
+    int updateVote(VoteDto vote);
+
+    // 투표 게시글 삭제
+    int deleteVote(String voteId);
+
+    // 투표 게시글 총 개수 조회 (페이징 처리용)
+    int countVoteList(Map<String, Object> paramMap);
 }
