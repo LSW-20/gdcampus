61982548a3ec37e9ce2afd260b438fd548058008
phn1389 <167429100+phn1389@users.noreply.github.com>
2024-12-02 08:21:17 +0900

    Notice 공지사항 등록 수정 삭제

--- src/main/java/com/br/gdcampus/service/NoticeService.java
+++ src/main/java/com/br/gdcampus/service/NoticeService.java
@@ -2,6 +2,8 @@ package com.br.gdcampus.service;
 
 import java.util.List;
 
+import com.br.gdcampus.dto.AttachDto;
+import com.br.gdcampus.dto.CommentDto;
 import com.br.gdcampus.dto.NoticeDto;
 
 /**
@@ -26,8 +28,27 @@ public interface NoticeService {
 	
 	// 다수의 번호들 가지고 공지사항 일괄삭제 
 	int deleteNotice(String[] deleteNo);
+
+	// 공지사항 게시글 상세 - 게시글 조회
+	NoticeDto selectNoticeDetail(String no);
+
+	// 공지사항 수정
+	int updatePost(NoticeDto n, String[] delFileNo);
 	
-	// 공지사항 일반게시글로 이동
+	// 공지사항 삭제
+	List<AttachDto> selectDelAttach(String[] delFileNo);
+	
+	// 공지사항 댓글조회기능
+	List<CommentDto> selectCommentList(String no);
+
+	int noticeUpdateIncreaseCount(String no);
+
+	List<AttachDto> selectAttachList(String no);
+
+	int deleteNotice(String no);
+
+	
+
 	
 
 	
